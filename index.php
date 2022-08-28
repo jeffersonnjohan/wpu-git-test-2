a6c1fd80dcfee4c546f5d13eb576681bf5924dc4
jeffersonnjohan <jeffersonnjohan@gmail.com>
2022-08-28 20:01:43 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet necessitatibus tempore sunt, provident voluptatem laboriosam voluptate dignissimos quod molestiae dolorem molestias quasi libero excepturi? Cupiditate inventore magni ipsum quos deleniti?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
